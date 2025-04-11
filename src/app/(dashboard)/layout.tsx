73fefb49b0cd181b003bda989a5bfc4a3ea16381
Pavel Cerveny <cervenypavel@gmail.com>
2025-04-11 16:13:46 +0200

    shared admin layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,6 +8,39 @@ import { auth } from "@/auth"
 import { cn } from "@/lib/utils"
 import SkipToMain from "@/components/admin/skip-to-main"
 import { AppSidebar } from "@/components/admin/layout/app-sidebar"
+import { Header } from '@/components/admin/layout/header'
+import { TopNav } from '@/components/admin/layout/top-nav'
+import { ProfileDropdown } from '@/components/admin/profile-dropdown'
+import { Search } from '@/components/admin/search'
+import ThemeSwitch from '@/components/theme-switch'
+import { Main } from '@/components/admin/layout/main'
+
+const topNav = [
+  {
+    title: 'Overview',
+    href: 'dashboard/overview',
+    isActive: true,
+    disabled: false,
+  },
+  {
+    title: 'Customers',
+    href: 'dashboard/customers',
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: 'Products',
+    href: 'dashboard/products',
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: 'Settings',
+    href: 'dashboard/settings',
+    isActive: false,
+    disabled: true,
+  },
+]
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const session = await auth()
@@ -32,7 +65,16 @@ export default async function DashboardLayout({ children }: { children: React.Re
             'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
           )}
         >
-          {children}
+          {/* ===== Top Heading ===== */}
+          <Header>
+            <TopNav links={topNav} />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <ThemeSwitch />
+              <ProfileDropdown />
+            </div>
+          </Header>
+          <Main>{children}</Main>
         </div>
       </SidebarProvider>
 
